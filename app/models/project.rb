d15be2d048240bdd10e8820c78de41af6fd69a2a
MrBrownser <castany.adria@gmail.com>
2015-02-02 15:35:21 +0100

    Practicing with configuring db seeds and using rake for setting up quickly

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ActiveRecord::Base
 	def self.iron_find(where_clause)
-		return Project.where(where_clause)
+		where(where_clause)
+	end
+
+	def self.clean_old
+		from = Time.now.midnight - 1.days
+		to = Time.now.midnight
+		where(updated_at: from..to).each do |project|
+			project.destroy
+		end
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create name: "Seed created 1", description: "Seed created description"
+
+Project.create name: "Seed created 2", description: "Another seed created project description"
+
+Project.create name: "Last seed created (3)", description: "I'm lazy"
