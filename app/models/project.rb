2cc0ae70231ab1d5f4247451addfdc3da7097e80
MrBrownser <castany.adria@gmail.com>
2015-02-04 11:04:51 +0100

    Practicing with forms

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,7 +14,26 @@ class Project < ActiveRecord::Base
 	end
 
 	def self.last_created_projects(n)
-		# limit(n).order("created_at") --> MORE PRO! CHECK THIS AFTER!!
-		Project.limit(n).order("created_at")
+		limit(n).order("created_at")
+		# Project.limit(n).order("created_at")
+	end
+
+	def total_hours_in_month(month, year)
+		m_first = Date.new(year, month, 1)
+		m_last = Date.new(year, month, -1)
+
+		month_entries = self.entries.where(date: m_first..m_last)
+
+		if !entries.empty?
+			t_hours = 0
+			t_minutes = 0
+			entries.each do |entry|
+				t_hours += entry.hours
+				t_minutes += entry.minutes
+			end
+			t_hours += t_minutes/60
+		else
+			0
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/contact' => 'site#contact'
   get '/projects' => 'projects#index'
   get '/projects/:id' => 'projects#show'
-  get '/projects/:project_id/entries' => 'entries#index'
-  get '/projects/:project_id/entries' => 'entries#show'
+  get '/projects/:project_id/entries' => 'entries#index', as: 'project_entries'
+  get '/projects/:project_id/entries/new' => 'entries#new'
+  get '/projects/:project_id/entries/:id' => 'entries#show'
+
+  post '/projects/:project_id/entries' => 'entries#create'
 end
